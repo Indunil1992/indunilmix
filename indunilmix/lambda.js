f8335a4a628d58f04f1b7e45bdc2644cfe4ad514
Indunil1992 <indunil@adroitlogic.com>
2019-07-05 10:43:04 +0530

    3

--- indunilmix/lambda.js
+++ indunilmix/lambda.js
@@ -10,17 +10,22 @@ exports.handler = function (event, context, callback) {
         QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/IndunilSQS.fifo`,
         DelaySeconds: '0',
         MessageDeduplicationId: '123',
-        MessageAttributes: {}
-
+        MessageAttributes: {
+            "@a": {
+                "DataType": "String",
+                "StringValue": "1"
+            }
+        },
+        MessageGroupId: '142'
     }, function (data) {
- console.log("success" + {data});
-            callback(null, "Successfully executed to enduser data" + { data });
+        console.log("success" + { data });
+        callback(null, "Successfully executed to enduser data" + { data });
         // your logic (logging etc) to handle successful message delivery, should be here
 
     }, function (error) {
         // your logic (logging etc) to handle failures, should be here
-        console.log("error " + {err});
-            callback(null, "Successfully executed to enduser catch" + { err });
+        console.log("error " + { err });
+        callback(null, "Successfully executed to enduser catch" + { err });
     });
 
     callback(null, { "message": "Successfully executed" });
